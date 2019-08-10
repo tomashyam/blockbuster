bd20013e92c55a8b2ffd53b76a9b16e7fc16c556
Unknown <refaelasraf@gmail.com>
2019-08-10 16:01:22 +0300

    add user data access

---
+++ CC/DAL/UserDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using CC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC.DAL
+{
+    public class UserDataAccess
+    {
+        private readonly CCContext _ccContext;
+
+        public UserDataAccess(CCContext context)
+        {
+            _ccContext = context;
+        }
+    }
+}
