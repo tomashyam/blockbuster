e9bb3563fc22054384e516ed35a0c6b1e642713d
Unknown <refaelasraf@gmail.com>
2019-08-10 17:57:38 +0300

    add controller for like and comments

---
+++ CC/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CC.BL;
+using CC.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly CommentBl _commentBl;
+
+
+        public CommentController(CCContext context)
+        {
+            _commentBl = new CommentBl(context);
+        }
+
+        [HttpPost]
+        public ActionResult Add(IFormCollection form)
+        {
+            _commentBl.Add(form["ProductId"], form["Comment"], HttpContext.Session.GetString("ConnectedUserId"));
+
+            return RedirectToAction("Details", "Product", new { id = form["ProductId"] });
+        }
+    }
+}
---
+++ CC/Controllers/LikeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CC.BL;
+using CC.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC.Controllers
+{
+    public class LikeController : Controller
+    {
+        private readonly LikeBl _likeBl;
+
+
+        public LikeController(CCContext context)
+        {
+            _likeBl = new LikeBl(context);
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] string data)
+        {
+            _likeBl.Add(data.ToString(), HttpContext.Session.GetString("ConnectedUserId"));
+
+            return RedirectToAction("Details", "Product", new { id = data });
+        }
+    }
+}
