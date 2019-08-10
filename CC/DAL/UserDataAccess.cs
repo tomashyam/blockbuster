909a6e1cb373de397a037ad457b279ff547efaac
Unknown <refaelasraf@gmail.com>
2019-08-10 16:05:22 +0300

    UserDataAccess finishes

--- CC/DAL/UserDataAccess.cs
+++ CC/DAL/UserDataAccess.cs
@@ -1,8 +1,10 @@
-﻿using CC.Data;
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using CC.Data;
+using CC.Models;
+
 
 namespace CC.DAL
 {
@@ -13,6 +15,55 @@ namespace CC.DAL
         public UserDataAccess(CCContext context)
         {
             _ccContext = context;
+        }
+
+        public void AddUser(string id, string name, Gender gender)
+        {
+            _ccContext.User.Add(new User
+            {
+                ID = id,
+                Name = name,
+                Gender = gender
+            });
+
+            _ccContext.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            _ccContext.User.Update(user);
+            _ccContext.SaveChanges();
+        }
+
+        public void DeletUser(User user)
+        {
+            var userToDelete = _ccContext
+                .User
+                .SingleOrDefault(u => u.ID == user.ID);
+
+            if (userToDelete == null)
+            {
+                throw new
+                    Exception(string.Format("could not find user with id {0}", user.ID));
+            }
+
+            _ccContext.User.Remove(userToDelete);
+            _ccContext.SaveChanges();
+        }
+
+        public bool IsUserExist(string id)
+        {
+            return _ccContext.User.Any(u => u.ID == id);
+        }
+
+        public User GetUser(string id)
+        {
+            return _ccContext.User.FirstOrDefault(u => u.ID == id);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _ccContext.User.ToList();
         }
     }
 }
