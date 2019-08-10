4c769d2126af26a666ea84e8e8627e40aaeaa681
Unknown <refaelasraf@gmail.com>
2019-08-10 17:50:54 +0300

    Add more BL'S

---
+++ CC/BL/CommentBl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CC.DAL;
+using CC.Data;
+
+namespace CC.BL
+{
+    public class CommentBl
+    {
+        private readonly ProductDataAccess _productDal;
+        private readonly UserDataAccess _userDal;
+
+        public CommentBl(CCContext context)
+        {
+            _productDal = new ProductDataAccess(context);
+            _userDal = new UserDataAccess(context);
+        }
+
+        public void Add(string productId, string text, string publisherId)
+        {
+            var comment = new Models.Comment
+            {
+                Text = text,
+                Date = DateTime.Now,
+                Publisher = _userDal.GetUser(publisherId)
+            };
+
+            _productDal.AddComment(int.Parse(productId), comment);
+        }
+    }
+}
---
+++ CC/BL/LikeBl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CC.DAL;
+using CC.Data;
+
+namespace CC.BL
+{
+    public class LikeBl
+    {
+        private readonly ProductDataAccess _productDal;
+        private readonly UserDataAccess _userDal;
+
+        public LikeBl(CCContext context)
+        {
+            _productDal = new ProductDataAccess(context);
+            _userDal = new UserDataAccess(context);
+        }
+
+        public void Add(string productId, string userId)
+        {
+            var like = new Models.Like
+            {
+                User = _userDal.GetUser(userId)
+            };
+
+            _productDal.AddLike(int.Parse(productId), like);
+        }
+    }
+}
