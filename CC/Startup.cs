0d9b4d3b48ce96cf0e511004a9b814810e66155e
Unknown <refaelasraf@gmail.com>
2019-08-10 19:52:57 +0300

    fix run time errors

--- CC/Startup.cs
+++ CC/Startup.cs
@@ -1,63 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CC.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using CC.Models;
 
 namespace CC
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+	public class Startup
+	{
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
+        public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+            var builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("ccContext"));
+            //{
+            //    Password = Configuration["Secret:DbPassword"]
+            //};
+            services.AddDbContext<CCContext>(options =>
+                                                options.UseSqlServer(builder.ConnectionString));
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            services.AddSingleton<ISecretSettings>(
+                // TODO: add secrests.
+                new SecretSettings(Configuration["Secret:MapCredantials"], 
+                                   Configuration["Secret:DbPassword"],
+                                   Configuration["Secret:WeatherKey"]));
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
             });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+		{ 
+			if (env.IsDevelopment())
+			{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+			app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
 
             app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
