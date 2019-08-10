528e3ece8d2304b84c26f046621460580cb60c65
Unknown <refaelasraf@gmail.com>
2019-08-10 15:51:40 +0300

    add functionalty for admin data access

--- CC/DAL/AdminDataAccess.cs
+++ CC/DAL/AdminDataAccess.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using CC.Data;
+using CC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CC.DAL
 {
     public class AdminDataAccess
     {
+        private readonly CCContext _ccContext;
+
+        public AdminDataAccess(CCContext ccContext)
+        {
+            _ccContext = ccContext;
+        }
+
+        public IEnumerable<Comment> GetByUserIdInDateRange(DateTime start, DateTime end, string userId)
+        {
+            var comments = _ccContext.Comment
+                .Include(usr => usr.Publisher)
+                .Where(c => c.Date >= start &&
+                       c.Date <= end &&
+                       c.Publisher.ID == userId);
+            return comments;
+        }
     }
 }
