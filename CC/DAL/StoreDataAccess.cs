642df5497d7ddf5f410e7053623854762dd00232
Unknown <refaelasraf@gmail.com>
2019-08-10 15:54:30 +0300

    add StoreDataAccess

---
+++ CC/DAL/StoreDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using CC.Data;
+using CC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CC.DAL
+{
+    public class StoreDataAccess
+    {
+        private readonly CCContext _ccContext;
+
+        public StoreDataAccess(CCContext ccContext)
+        {
+            _ccContext = ccContext;
+        }
+
+    }
+}
